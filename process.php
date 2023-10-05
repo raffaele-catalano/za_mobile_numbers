2b16e2ae7b0a99313262d1eb2fa4fd83cee635a5
raffaele-catalano <raf.catalano@gmail.com>
2023-10-05 18:27:17 +0200

    made several operations: ↓ - included database.php - set up connection with db - added stmt operations

--- process.php
+++ process.php
@@ -2,22 +2,31 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_FILES["file"])) {
         $file = $_FILES["file"];
-        
+
         if ($file["error"] === UPLOAD_ERR_OK) {
             $tempFilePath = $file["tmp_name"];
-            
+
             $fileType = mime_content_type($tempFilePath);
             if ($fileType === "text/csv" || $fileType === "application/csv" || $fileType === "application/vnd.ms-excel") {
                 $csvData = file_get_contents($tempFilePath);
-                
+
                 $csvRows = explode("\n", $csvData);
-                
+
                 $isFirstRow = true;
 
                 $validNumberSum = [];
                 $updatedNumberSum = [];
                 $invalidNumberSum = [];
-                
+
+                include 'database.php';
+
+                try {
+                    $db = new PDO('sqlite:za_numbers.db');
+                    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                } catch (PDOException $e) {
+                    die("Errore nella connessione al database: " . $e->getMessage());
+                }
+
                 foreach ($csvRows as $row) {
                     $row = trim($row);
 
@@ -27,33 +36,57 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     }
 
                     $fields = explode(",", $row);
-
                     $phoneNumber = trim($fields[1]);
 
                     $validPattern = '/^27\d{9}$/';
                     $updatedPattern = '/^27\d{9}_DELETED_\d+$/';
-                    
+
                     if (preg_match($validPattern, $phoneNumber)) {
                         $validNumber = $phoneNumber;
                         array_push($validNumberSum, $validNumber);
                         echo "Numero valido: $phoneNumber<br>";
+
+                        $stmt = $db->prepare("INSERT INTO numeri (tipo, numero) VALUES (:tipo, :numero)");
+                        $stmt->bindParam(':tipo', $tipo);
+                        $stmt->bindParam(':numero', $numero);
+
+                        $tipo = 'valido';
+                        $numero = $validNumber;
+                        $stmt->execute();
                     } elseif (preg_match($updatedPattern, $phoneNumber)) {
                         $updatedNumber = $phoneNumber;
                         array_push($updatedNumberSum, $updatedNumber);
                         echo "Numero aggiornato: $phoneNumber<br>";
-                    }
-                    else {
+
+                        $stmt = $db->prepare("INSERT INTO numeri (tipo, numero) VALUES (:tipo, :numero)");
+                        $stmt->bindParam(':tipo', $tipo);
+                        $stmt->bindParam(':numero', $numero);
+
+                        $tipo = 'aggiornato';
+                        $numero = $updatedNumber;
+                        $stmt->execute();
+                    } else {
                         $invalidNumber = $phoneNumber;
                         array_push($invalidNumberSum, $invalidNumber);
                         echo "Numero non valido: $phoneNumber<br>";
+
+                        $stmt = $db->prepare("INSERT INTO numeri (tipo, numero) VALUES (:tipo, :numero)");
+                        $stmt->bindParam(':tipo', $tipo);
+                        $stmt->bindParam(':numero', $numero);
+
+                        $tipo = 'non valido';
+                        $numero = $invalidNumber;
+                        $stmt->execute();
                     }
                 }
+                
+                // chiudere la connessione al database
+                $db = null;
+                
                 echo "<br>";
-                echo "Somma dei numeri validi:" . count($validNumberSum);
-                echo "<br>";
-                echo "Somma dei numeri aggiornati:" . count($updatedNumberSum);
-                echo "<br>";
-                echo "Somma dei numeri non validi:" . count($invalidNumberSum);
+                echo "Somma dei numeri validi: " . count($validNumberSum) . "<br>";
+                echo "Somma dei numeri aggiornati: " . count($updatedNumberSum) . "<br>";
+                echo "Somma dei numeri non validi: " . count($invalidNumberSum) . "<br>";
             } else {
                 echo "Il file deve essere un CSV valido.";
             }
