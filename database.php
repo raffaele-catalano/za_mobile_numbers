556df9b978627ff05f73ba7d8967fef8f4f3c682
raffaele-catalano <raf.catalano@gmail.com>
2023-10-05 23:13:32 +0200

    created a new table REGISTRO: ↓ - in this table, all changes made to the records in the 'numeri' table will be recorded; - "numeri" and "registro" have a one-to-many relation.

--- database.php
+++ database.php
@@ -3,11 +3,20 @@ try {
     $db = new PDO('sqlite:za_numbers.db');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    // Crea la tabella numeri se non esiste già
+    // Tabella dei numeri
     $db->exec("CREATE TABLE IF NOT EXISTS numeri (
         id INTEGER PRIMARY KEY,
         tipo TEXT,
         numero TEXT
+    )");
+
+    // Tabella delle modifiche
+    $db->exec("CREATE TABLE IF NOT EXISTS registro (
+        id INTEGER PRIMARY KEY,
+        id_num INTEGER,
+        dettagli TEXT,
+        data_modifica DATETIME,
+        FOREIGN KEY (id_num) REFERENCES numeri (id)
     )");
 } catch (PDOException $e) {
     die("Errore nella connessione al database: " . $e->getMessage());
