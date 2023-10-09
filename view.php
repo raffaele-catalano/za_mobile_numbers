e6de3f51f4b285884a71bc91a491b1e11063bc24
raffaele-catalano <raf.catalano@gmail.com>
2023-10-09 10:33:09 +0200

    added pagination to numeri table and styled it

--- view.php
+++ view.php
@@ -7,6 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- favicon -->
     <link rel="shortcut icon" href="./assets/img/za_favicon.png" type="png">
+    <!-- fontawesome -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css' integrity='sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==' crossorigin='anonymous'/>
     <!-- style -->
     <link rel="stylesheet" href="./assets/styles/style.css">
     <!--  -->
@@ -29,21 +31,34 @@
         <div class="row">
             <div class="col">
                 <h3>Numeri</h3>
-                <table id="numberTable" class="table table-striped">
-
+                <table id="numberTable" class="table table-dark table-striped w-50">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
                             <th scope="col">Numero</th>
                         </tr>
                     </thead>
-
-                    <!-- <tbody id="tbody"> -->
-                    <tbody>
-                        <!-- dovrebbe esser riempito dalla funzione js -->
+                    <tbody id="tbody">
+                        <!-- qui vengono inseriti i dati tramite JavaScript -->
                     </tbody>
-
                 </table>
+                <!-- pagination -->
+                <nav aria-label="...">
+                    <ul class="pagination">
+                        <li class="page-item" id="prevPage">
+                            <a class="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true"><i class="fa-solid fa-backward"></i></span>
+                            </a>
+                        </li>
+                        <!-- Le pagine verranno create dinamicamente qui -->
+                        <li class="page-item" id="nextPage">
+                            <a class="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true"><i class="fa-solid fa-forward"></i></span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <!--  -->
             </div>
         </div>
     </div>
@@ -55,6 +70,10 @@
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <!--  -->
     <script>
+        const itemsPerPage = 15;
+        let currentPage = 1;
+        let dataToDisplay = [];
+
         // funzione per ottenere e mostrare i numeri in base al filtro selezionato
         function loadNumbers(filter) {
             axios.get('api.php', {
@@ -63,28 +82,71 @@
                 }
             })
             .then(response => {
-                const tableBody = document.querySelector("#numberTable tbody");
-                tableBody.innerHTML = '';
-                
-                if (response.data.error) {
-                    tableBody.innerHTML = `<tr><td colspan="2">${response.data.error}</td></tr>`;
-                } else {
-                    response.data.forEach((numero, index) => {
-                        tableBody.innerHTML += `<tr><td>${index + 1}</td><td>${numero}</td></tr>`;
-                    });
-                }
+                dataToDisplay = response.data;
+                currentPage = 1;
+                renderData();
             })
             .catch(error => console.error('Errore durante il caricamento dei numeri:', error));
         }
-        
+
+        // cambio di filtro
         document.getElementById("filterSelect").addEventListener("change", function () {
             const selectedFilter = this.value;
             loadNumbers(selectedFilter);
         });
-        
-        // carica tutti i numeri all'avvio
+
+        // Listener per il click sulla paginazione precedente
+        document.getElementById("prevPage").addEventListener("click", () => {
+            if (currentPage > 1) {
+                currentPage--;
+                renderData();
+            }
+        });
+
+        // click sulla paginazione successiva
+        document.getElementById("nextPage").addEventListener("click", () => {
+            const totalPages = Math.ceil(dataToDisplay.length / itemsPerPage);
+            if (currentPage < totalPages) {
+                currentPage++;
+                renderData();
+            }
+        });
+
+        // rendering dei dati in base alla pagina corrente
+        function renderData() {
+            const start = (currentPage - 1) * itemsPerPage;
+            const end = start + itemsPerPage;
+            const slicedData = dataToDisplay.slice(start, end);
+
+            const tableBody = document.querySelector("#tbody");
+            tableBody.innerHTML = '';
+
+            slicedData.forEach((numero, index) => {
+                const rowNumber = start + index + 1;
+                tableBody.innerHTML += `<tr><td>${rowNumber}</td><td>${numero}</td></tr>`;
+            });
+
+            // calcola il numero totale di pagine
+            const totalPages = Math.ceil(dataToDisplay.length / itemsPerPage);
+
+            // abilitazione/disabilitazione dei pulsanti Prev e Next
+            const prevPageButton = document.getElementById("prevPage");
+            const nextPageButton = document.getElementById("nextPage");
+            if (currentPage === 1) {
+                prevPageButton.classList.add("disabled");
+            } else {
+                prevPageButton.classList.remove("disabled");
+            }
+            if (currentPage === totalPages) {
+                nextPageButton.classList.add("disabled");
+            } else {
+                nextPageButton.classList.remove("disabled");
+            }
+        }
+
+        // Carica tutti i numeri all'avvio con filtro "all"
         loadNumbers("all");
-        
+
     </script>
 </body>
 </html>
