75fe56bccf9cf7ba35c28cbceb1a9ac5d160f320
raffaele-catalano <raf.catalano@gmail.com>
2023-10-09 10:32:15 +0200

    commented DROP TABLE

--- database.php
+++ database.php
@@ -3,8 +3,8 @@ try {
     $db = new PDO('sqlite:za_numbers.db');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    $db->exec("DROP TABLE IF EXISTS numeri");
-    $db->exec("DROP TABLE IF EXISTS registro");
+    // $db->exec("DROP TABLE IF EXISTS numeri");
+    // $db->exec("DROP TABLE IF EXISTS registro");
 
     // Tabella dei numeri
     $db->exec("CREATE TABLE IF NOT EXISTS numeri (
